c7bdf9eafbb9b09d638e030f07138583ebf5d1cb
koyanagi0709 <koyanagi0709@gmail.com>
2026-06-23 06:15:30 +0900

    文字列について学習

---
+++ str/01.php
@@ -0,0 +1,6 @@
+<?php
+# 文字リテラルについて学習
+
+print 'You are "GREAT" teacher!!';
+print PHP_EOL;
+print 'He\'s "GREAT" teacher!!'; #エスケープ処理
---
+++ string.php
@@ -0,0 +1,8 @@
+<?php
+$title = 'サーバーサイド技術の学び舎 - WINGS';
+$data1 = "サポートサイト\t「{$title}」へ".PHP_EOL;
+$data2 = 'サポートサイト\t「{$title}」へ'.PHP_EOL;
+$data3 = "aaaaa${title}aaaaaaaaa"."\n";
+print $data1;
+print $data2;
+print $data3;
